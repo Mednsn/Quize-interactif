8d64034a1079373be9bfcdff0d38db5cff685bed
mohamed <medolgholb@gmail.com>
2025-11-06 17:13:10 +0100

    ajouter listes des resulats

--- quiz1.js
+++ quiz1.js
@@ -3,7 +3,7 @@ const QUESTIONS = [
   ["Combien font 5 + 3 ?", "3", "6", "9", "8", "8"],
   ["Quelle est la couleur du ciel par temps clair ?", "vers", "bleu", "noir", "rouge", "bleu"],
 
-  ["Quelle planète est la plus proche du Soleil ?", "Mercure", "tére", "moon", "Mercured", "Mercure"]
+  ["Quelle planète est la plus proche du Soleil ?", "Mercure", "tere", "moon", "Mercured", "Mercure"]
   // ["Quel est le fleuve le plus long du monde ?", "draa", "dahir", "Nil", "kotliner", "Nil"]
 ];
 // ["Quel est le plus grand océan ?", "atlantique", "indien", "arctique", "Pacifique", "Pacifique"],
@@ -17,7 +17,8 @@ const QUESTIONS = [
 // ["Quel est le plus grand pays du monde ?", "maroc", "okran", "nighterant", "Russie", "Russie"],
 // ["De quelle couleur est une banane mûre ?", "rouge", "noir", "jaune", "vere", "jaune"],
 
-
+var reponseCorrect = 0;
+let tabValue = [];
 var bonnesReponse = 0;
 var k = 0, i = 0;
 var buttonPrecedant = document.getElementById("Button-precedant");
@@ -25,40 +26,29 @@ var buttonSuivant = document.getElementById("Button-suivant");
 
 document.getElementById("deuxeme-Section").style.visibility = "hidden"
 
-
 document.getElementById("question").innerText = QUESTIONS[0][0]
 document.getElementById("choix11").innerText = QUESTIONS[0][1]
 document.getElementById("choix22").innerText = QUESTIONS[0][2]
 document.getElementById("choix33").innerText = QUESTIONS[0][3]
 document.getElementById("choix44").innerText = QUESTIONS[0][4]
 
-
-
 buttonPrecedant.style.visibility = "hidden";
-
-
-
-
-
-
-
-
-
-
-//function checkign()***********************************
-var conteur = 0;
+//function checkign()***************************************************************************************
 function checkingFUNC() {
-
   const checkedup = document.querySelectorAll(".choix");
-  var reponseCorrect = 0;
-
-  checkedup.forEach(radio=> {
-
+  checkedup.forEach((radio, index) => {
+    radio.value = QUESTIONS[i][index + 1];
     if (radio.checked) {
+
+      tabValue.push(radio.value);
+
+
       let outputRV = radio.value;
       let outputQS = QUESTIONS[i][5];
       if (outputRV === outputQS) {
+        console.log(reponseCorrect);
         reponseCorrect++;
+        document.getElementById("div-h2-score").textContent = "votre resultats est : " + reponseCorrect + "/15.";
       }
       k++;
       buttonPrecedant.style.visibility = "visible"
@@ -72,95 +62,75 @@ function checkingFUNC() {
       // 15
       if (i == 4) {
         terminer();
-
       }
-
       document.getElementById("question").innerText = QUESTIONS[i][0]
       document.getElementById("choix11").innerText = QUESTIONS[i][1]
       document.getElementById("choix22").innerText = QUESTIONS[i][2]
       document.getElementById("choix33").innerText = QUESTIONS[i][3]
       document.getElementById("choix44").innerText = QUESTIONS[i][4]
-
       radio.checked = false;
-
     }
-
   });
- document.getElementById("div-h2-score").textContent=  "votre resultats est : "+reponseCorrect+"/15." ;
 
 }
-
-
+//**************************************************************************************************************
 btnSuivant()
-//***********************************
-
 function btnSuivant() {
-
   buttonSuivant.addEventListener('click', function () {
     checkingFUNC()
   }
   )
 }
-
-//************************************ 
+//************************************ *********************************************************************
 btnPrec()
-
 function btnPrec() {
-
   buttonPrecedant.addEventListener('click', function () {
-
     i--;
     k--;
     buttonSuivant.innerText = "suivant"
     if (k == 0) {
       buttonPrecedant.style.visibility = "hidden"
     }
-
     document.getElementById("question").innerText = QUESTIONS[i][0]
     document.getElementById("choix11").innerText = QUESTIONS[i][1]
     document.getElementById("choix22").innerText = QUESTIONS[i][2]
     document.getElementById("choix33").innerText = QUESTIONS[i][3]
     document.getElementById("choix44").innerText = QUESTIONS[i][4]
-
-
-
   })
 }
-// !!***************************************
+// !!***************************************!ùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùùù**********************************
 function terminer() {
-
   document.getElementById("deuxeme-Section").style.visibility = "visible"
-
   var afficherDiv = document.getElementById("div-affichage");
   var conteneurDiv = document.getElementById("div-conteneur");
-
   for (let i = 0; i < QUESTIONS.length; i++) {
-
-
     const clone = afficherDiv.cloneNode(true);
     var questionSct2 = clone.querySelector(".Deux-Section-H2");
     var repnse = clone.querySelectorAll(".h33");
     questionSct2.textContent = QUESTIONS[i][0];
     repnse.forEach((raio, index) => {
       raio.textContent = QUESTIONS[i][index + 1];
-
+      raio.value = QUESTIONS[i][index + 1];
     });
 
+    if (tabValue[i] != QUESTIONS[i][5]) {
+
+      for (let j = 0; j < 4; j++) {
+        if (tabValue[i] === repnse[j].value) {
+
+          repnse[j].style.background = 'red';
+        }
+      }
+    }
+    for (let p = 0; p < 4; p++) {
+      if (repnse[p].value === QUESTIONS[i][5]) {
+
+        repnse[p].style.background = 'Lime';
+      }
+    }
 
     conteneurDiv.appendChild(clone);
-
   }
 }
-///****************************** */
-ajouterValue()
-function ajouterValue() {
-  const checkedup = document.getElementsByName('choix');
-  for (let i = 0; i < QUESTIONS.length; i++) {
-    checkedup.forEach((radio, index) => {
-
-      radio.value = QUESTIONS[i][index + 1]
-    })
-  }
-}
-// !!!!!!**********************************!!!!!!!!!!!!!!
+// **********************************************************************************************************************
 
